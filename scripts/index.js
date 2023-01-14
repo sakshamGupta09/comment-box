81e86bdd4706cad59929b61c12e021e460a1dcbc
Saksham Gupta <saksham.09.gupta@gmail.com>
2023-01-14 07:56:26 +0530

    UI improvements and Refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -18,39 +18,77 @@ postContainerElement.addEventListener("click", postEventsHandler);
 // Functions
 
 function postEventsHandler(e) {
-  // Post reply button
-  if (e.target.nodeName === "BUTTON" && e.target.name === "post-comment") {
+  // Comment button
+  if (e.target.nodeName === "BUTTON" && e.target.name === btnType.postComment) {
     const comment = getCommentValue(e);
-    comment && postComment(e, comment);
+    comment && postComment(comment);
     return;
+  }
+  // Reply button
+  if (e.target.nodeName === "BUTTON" && e.target.name === btnType.postReply) {
   }
 }
 
 // Utilities
 
+function getNestedRepliesNode() {
+  const DIV = document.createElement("div");
+  DIV.classList.add("replies");
+  return DIV;
+}
+
+function getCommentBoxNode() {
+  const DIV = document.createElement("div");
+  DIV.classList.add("comment__box");
+
+  const INPUT = document.createElement("input");
+  INPUT.setAttribute("type", "text");
+  INPUT.setAttribute("name", "comment-input");
+  INPUT.setAttribute("placeholder", "Add a comment ...");
+  INPUT.classList.add("comment__input");
+
+  const BUTTON = document.createElement("button");
+  BUTTON.setAttribute("name", "post-comment");
+  BUTTON.classList.add("btn", "btn--primary");
+  BUTTON.textContent = "Post";
+
+  DIV.append(INPUT, BUTTON);
+  return DIV;
+}
+
 function getCommentValue(e) {
   const inputElement = e.target.previousElementSibling;
+  let value = "";
   if (
     inputElement?.nodeName === "INPUT" &&
     inputElement?.name === "comment-input"
   ) {
-    return inputElement.value;
+    value = inputElement.value;
+    inputElement.value = "";
   }
-  return "";
+  return value;
 }
 
 function getCommentNode(commentValue) {
+  const ARTICLE = document.createElement("article");
+  ARTICLE.classList.add("comment__wrapper");
+
   const DIV = document.createElement("div");
   DIV.classList.add("comment");
   DIV.textContent = commentValue;
 
-  return DIV;
+  const BUTTON = document.createElement("button");
+  BUTTON.textContent = "Reply";
+  BUTTON.classList.add("btn", "btn--secondary");
+  BUTTON.name = "post-reply";
+
+  ARTICLE.append(DIV, BUTTON);
+
+  return ARTICLE;
 }
 
-function postComment(e, comment) {
-  if (e.target.name === btnType.postComment) {
-    const commentNode = getCommentNode(comment);
-    commentsContainerElement.prepend(commentNode);
-    return;
-  }
+function postComment(comment, isNestedComment = false) {
+  const commentNode = getCommentNode(comment);
+  commentsContainerElement.prepend(commentNode);
+  return;
 }
